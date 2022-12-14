da36642b2c39f614e230098361b9ee621d995f76
AJ Holland <aholland95@gmail.com>
2022-12-13 21:23:55 -0600

    first commit

---
+++ models/Todo.js
@@ -0,0 +1,16 @@
+// modify for later
+
+const mongoose = require('mongoose')
+
+const TodoSchema = new mongoose.Schema({
+   todo: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+  }
+})
+
+module.exports = mongoose.model('Todo', TodoSchema)
